f92d6e6339b242780b55558780ee95371928efc0
Maksim Berdnikau <max.berd.1900@gmail.com>
2024-10-09 21:51:27 +0300

    Добавление Nuget-пакетов в зависимости проекта

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace ConsoleApp
+﻿namespace ConsoleApp;
+
+public class Program
 {
-	internal class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello, World!");
-		}
+		Console.WriteLine("Hello, World!");
 	}
 }
